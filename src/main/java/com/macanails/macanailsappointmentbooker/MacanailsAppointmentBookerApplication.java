89e4633436c669b4b976cb43f6b2a8e018913e05
NoemiSz <szokenoemijob@gmail.com>
2019-05-21 08:02:38 +0200

    initial commit

---
+++ src/main/java/com/macanails/macanailsappointmentbooker/MacanailsAppointmentBookerApplication.java
@@ -0,0 +1,13 @@
+package com.macanails.macanailsappointmentbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacanailsAppointmentBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacanailsAppointmentBookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macanails/macanailsappointmentbooker/MacanailsAppointmentBookerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.macanails.macanailsappointmentbooker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MacanailsAppointmentBookerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
