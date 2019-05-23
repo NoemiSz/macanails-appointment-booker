2cc10e187eccf7fe469f4508e608c025de169c57
NoemiSz <szokenoemijob@gmail.com>
2019-05-23 10:13:23 +0200

    createForm test with basic FormService's createForm method

--- src/main/java/com/macanails/macanailsappointmentbooker/service/FormService.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/service/FormService.java
@@ -1,5 +1,18 @@
 package com.macanails.macanailsappointmentbooker.service;
 
-public class FormService {
+import com.macanails.macanailsappointmentbooker.model.Form;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
+@Service
+public class FormService {
+    @Autowired
+    Form form;
+
+    public Form createForm(Map<String, Object> answers) {
+        Form form = new Form();
+        return form;
+    }
 }
--- src/test/java/com/macanails/macanailsappointmentbooker/model/FormTest.java
+++ src/test/java/com/macanails/macanailsappointmentbooker/model/FormTest.java
@@ -1,14 +1,45 @@
 package com.macanails.macanailsappointmentbooker.model;
 
+import com.macanails.macanailsappointmentbooker.service.FormService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
 class FormTest {
+    @Autowired
+    FormService formService;
+    @Autowired
+    Form form;
+
     @Test
     public void existenceCheck() {
-        Form form = new Form();
+        form = new Form();
+        assertThat(form).isNotNull();
+    }
+
+    @Test
+    public void createForm() {
+        Map<String, Object> answers = new HashMap();
+        List<String> list = new ArrayList(){
+            {
+                add("a");
+                add("e");
+                add("g");
+            }
+        };
+        answers.put("answers",list);
+        formService = new FormService();
+        form = formService.createForm(answers);
         assertThat(form).isNotNull();
     }
 
