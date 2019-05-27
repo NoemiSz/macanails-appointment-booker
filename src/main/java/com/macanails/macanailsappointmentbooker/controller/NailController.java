edbb18041d37ffdc346ecfc34dc2090352cc6c4a
NoemiSz <szokenoemijob@gmail.com>
2019-05-27 19:44:21 +0200

    FormController was renamed and receives new JSON from frontend

--- src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
@@ -1,27 +1,26 @@
 package com.macanails.macanailsappointmentbooker.controller;
 
 
-import com.macanails.macanailsappointmentbooker.service.FormService;
+import com.macanails.macanailsappointmentbooker.service.NailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @CrossOrigin
-public class FormController {
+public class NailController {
     @Autowired
-    FormService formService;
+    NailService nailService;
 
     @PostMapping(value = "/")
-    public Map<String, List<String>> getParameters(@RequestBody Map<String, List<String>> answers) {
+    public Map<String, String> getParameters(@RequestBody Map<String, String> answers) {
 
         return answers;
     }
 
     @PostMapping(value = "/personal")
-    public Map<String, List<String>> getPersonal(@RequestBody Map<String, List<String>> personal) {
+    public Map<String, String> getPersonal(@RequestBody Map<String, String> personal) {
 
         return personal;
     }
