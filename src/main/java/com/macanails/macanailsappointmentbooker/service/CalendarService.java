49ba2c7898695a392c51d9b567f5d98ea0d5b1b6
Szajler Indira <szajler.indira@gmail.com>
2019-05-30 06:15:43 +0200

    updateEvent() added

--- src/main/java/com/macanails/macanailsappointmentbooker/service/CalendarService.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/service/CalendarService.java
@@ -1,14 +1,14 @@
 package com.macanails.macanailsappointmentbooker.service;
 
 import com.google.api.client.util.DateTime;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
+import com.google.api.services.calendar.model.*;
 import com.macanails.macanailsappointmentbooker.model.CalendarEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -41,6 +41,61 @@ public class CalendarService {
 
         }
         return items;
+    }
+
+    public void addNewEvent(CalendarEvent calendarEvent) throws IOException {
+
+        Event event = new Event()
+                .setSummary("Köröm")
+                .setLocation("Szalon")
+                .setDescription(calendarEvent.getDescription());
+
+        DateTime startDateTime = new DateTime("2019-05-24T19:00:00");
+        EventDateTime start = new EventDateTime()
+                .setDateTime(startDateTime)
+                .setTimeZone("Europe/Budapest");
+
+        event.setStart(start);
+
+        DateTime endDateTime = new DateTime("2019-05-24T21:00:00");
+        EventDateTime end = new EventDateTime()
+                .setDateTime(endDateTime)
+                .setTimeZone("Europe/Budapest");
+
+
+        event.setEnd(end);
+
+
+        EventAttendee[] attendees = new EventAttendee[] {
+                new EventAttendee().setEmail("macanailstest@gmail.com"),
+                new EventAttendee().setEmail("szajler.indira@gmail.com"),
+        };
+        event.setAttendees(Arrays.asList(attendees));
+
+
+        EventReminder[] reminderOverrides = new EventReminder[] {
+                new EventReminder().setMethod("email").setMinutes(24 * 60),
+
+        };
+        Event.Reminders reminders = new Event.Reminders()
+                .setUseDefault(false)
+                .setOverrides(Arrays.asList(reminderOverrides));
+        event.setReminders(reminders);
+
+        String calendarId = "primary";
+        event = calendarConnection.service.events().insert(calendarId, event).execute();
+        System.out.printf("Event created: %s\n", event.getHtmlLink());
+
+    }
+
+    public void updateEvent(CalendarEvent calendarEvent) throws IOException {
+        Event event = calendarConnection.service.events().get("primary", calendarEvent.getId()).execute();
+
+
+        event.setDescription("test");
+
+
+        Event updatedEvent = calendarConnection.service.events().update("primary", event.getId(), event).execute();
     }
 
 }
