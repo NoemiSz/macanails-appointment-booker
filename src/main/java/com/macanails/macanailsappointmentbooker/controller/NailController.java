6b351fa245fefbc98ca3c8609ade7782f1d76ac1
NoemiSz <szokenoemijob@gmail.com>
2019-05-31 13:41:16 +0200

    NailController getFreeSlots method is done.

--- src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
@@ -1,10 +1,15 @@
 package com.macanails.macanailsappointmentbooker.controller;
 
 
+import com.macanails.macanailsappointmentbooker.model.CalendarEvent;
+import com.macanails.macanailsappointmentbooker.model.Nail;
 import com.macanails.macanailsappointmentbooker.service.NailService;
+import com.macanails.macanailsappointmentbooker.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -12,16 +17,23 @@ import java.util.Map;
 public class NailController {
     @Autowired
     NailService nailService;
+    @Autowired
+    Nail nail;
+    @Autowired
+    ReservationService reservationService;
 
-    @PostMapping(value = "/")
-    public Map<String, String> getParameters(@RequestBody Map<String, String> answers) {
-
-        return answers;
+    @GetMapping(value = "/")
+    public Map<String, List<CalendarEvent>> getFreeSlots(@RequestBody Map<String, String> answers) {
+        nail = nailService.createNail(answers);
+        List<CalendarEvent> freeSlots = reservationService.getFreeSlots(nail);
+        Map <String, List<CalendarEvent>> freeSlotsMap = new HashMap<String, List<CalendarEvent>>();
+        freeSlotsMap.put("freeSlots",freeSlots );
+        return freeSlotsMap;
     }
 
-    @PostMapping(value = "/personal")
-    public Map<String, String> getPersonal(@RequestBody Map<String, String> personal) {
-
-        return personal;
-    }
+//    @PostMapping(value = "/personal")
+//    public Map<String, String> getPersonal(@RequestBody Map<String, String> personal) {
+//
+//        return personal;
+//    }
 }
