7ca743f61bbd228f7869a84d34526f69ef275355
Szajler Indira <szajler.indira@gmail.com>
2019-06-26 10:20:47 +0200

    test method removed

--- src/main/java/com/macanails/macanailsappointmentbooker/service/CalendarService.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/service/CalendarService.java
@@ -3,14 +3,10 @@ package com.macanails.macanailsappointmentbooker.service;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.*;
 import com.macanails.macanailsappointmentbooker.model.CalendarEvent;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,34 +22,12 @@ public class CalendarService {
     @Autowired
     ReservationService reservationService;
 
-    @PostConstruct
-    public void test() throws IOException {
-        CalendarEvent eventt= new CalendarEvent();
-        eventt.setNeededTime(2);
-               eventt .setStartTime(LocalDateTime.of(2019, 6, 4, 10, 0));
-               eventt.setDescription("test");
-               eventt.setId(getFreeEvents(DateTimeService.convertLocalDateTimeToDateTime(eventt.getStartTime()),
-                       DateTimeService.convertLocalDateTimeToDateTime(eventt.getStartTime().plusHours(eventt.getNeededTime()))).get(0).getId());
-
-
-        System.out.println(eventt.toString());
-
-        System.out.println(reservationService.saveAppointment(eventt));
-
-//        DateTime time = new DateTime(System.currentTimeMillis());
-//        LocalDateTime now = DateTimeService.convertDateTimeToLocalDateTime(time).withSecond(0).withNano(0);
-//
-//        LocalDateTime date = calculateDateTime(2, LocalDateTime.now().withNano(0).withSecond(0));
-//        List<CalendarEvent> freeEvents =getFreeEvents(DateTimeService.convertLocalDateTimeToDateTime(now), DateTimeService.convertLocalDateTimeToDateTime(date));
-//        updateEvent(freeEvents.get(0), 1);
-//        deleteEvent(freeEvents.get(1));
-    }
 
     public List<CalendarEvent> getFreeEvents(DateTime min, DateTime max) throws IOException {
         List<CalendarEvent> items = new ArrayList<>();
 
         Events events = calendarConnection.service.events().list("primary")
-                .setMaxResults(20)
+                .setMaxResults(80)
                 .setTimeMin(min)
                 .setTimeMax(max)
                 .setOrderBy("startTime")
@@ -110,13 +84,14 @@ public class CalendarService {
 
     public void updateEvent(CalendarEvent calendarEvent, int neededHours) throws IOException {
         Event event = calendarConnection.service.events().get("primary", calendarEvent.getId()).execute();
+//        addAttendees(event, calendarEvent.getCustomer().getEmail());
+        calendarConnection.service.events().update("primary", event.getId(), event).execute();
 
         modifyEndTime(event,calendarEvent, neededHours);
-//        addAttendees(event, calendarEvent.getCustomer().getEmail());
-//        addReminders(event);
-//        event.setDescription(calendarEvent.getDescription())
-//                .setLocation("Szalon")
-//                .setGuestsCanModify(false);
+        addReminders(event);
+        event.setDescription(calendarEvent.getDescription())
+                .setLocation("Szalon")
+               .setGuestsCanModify(false);
 
         calendarConnection.service.events().update("primary", event.getId(), event).execute();
     }
@@ -154,19 +129,5 @@ public class CalendarService {
                 .setTimeZone("Europe/Budapest");
         event.setEnd(eventEndTime);
 
-    }
-
-
-
-
-
-
-
-    private LocalDateTime calculateDateTime(int neededHours,  LocalDateTime dateTime) {
-
-        return dateTime.plusHours(neededHours);
-    }
-
-    public void saveEvent() {
     }
 }
