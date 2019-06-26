ddf740f8d72c2341743cebf51196fff93325bf72
Szajler Indira <szajler.indira@gmail.com>
2019-06-26 10:23:15 +0200

    IOException added to method head

--- src/main/java/com/macanails/macanailsappointmentbooker/controller/EventController.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/controller/EventController.java
@@ -1,29 +1,27 @@
 package com.macanails.macanailsappointmentbooker.controller;
 
 import com.macanails.macanailsappointmentbooker.model.CalendarEvent;
-import com.macanails.macanailsappointmentbooker.model.Nail;
-import com.macanails.macanailsappointmentbooker.service.CalendarService;
-import com.macanails.macanailsappointmentbooker.service.NailService;
 import com.macanails.macanailsappointmentbooker.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
 @CrossOrigin
 public class EventController {
+        @Autowired
+        CalendarEvent calendarEvent;
 
         @Autowired
-        CalendarService calendarService;
+        ReservationService reservationService;
 
         @PostMapping(value = "/")
-        public void saveEvent(@RequestBody Map<String, String> answers) {
+        public void saveEvent(@RequestBody Map<String, String> answers) throws IOException {
 
         //TODO
-            calendarService.saveEvent();
+            reservationService.saveAppointment(calendarEvent);
         }
 
     }
--- src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
@@ -3,11 +3,13 @@ package com.macanails.macanailsappointmentbooker.controller;
 
 import com.macanails.macanailsappointmentbooker.model.CalendarEvent;
 import com.macanails.macanailsappointmentbooker.model.Nail;
+import com.macanails.macanailsappointmentbooker.service.CalendarService;
 import com.macanails.macanailsappointmentbooker.service.NailService;
 import com.macanails.macanailsappointmentbooker.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,9 +24,13 @@ public class NailController {
     @Autowired
     ReservationService reservationService;
 
+    @Autowired
+    CalendarService calendarService;
+
     @GetMapping(value = "/")
-    public Map<String, List<CalendarEvent>> getFreeSlots(@RequestBody Map<String, String> answers) {
+    public Map<String, List<CalendarEvent>> getFreeSlots(@RequestBody Map<String, String> answers) throws IOException {
         nail = nailService.createNail(answers);
+
         List<CalendarEvent> freeSlots = reservationService.getFreeSlots(nail);
         Map <String, List<CalendarEvent>> freeSlotsMap = new HashMap<String, List<CalendarEvent>>();
         freeSlotsMap.put("freeSlots",freeSlots );
