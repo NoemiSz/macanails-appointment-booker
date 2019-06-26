8266fc7314f0dd542bfef5a48fbe273aafaab339
Szajler Indira <szajler.indira@gmail.com>
2019-06-26 10:18:25 +0200

    getFreeSlots(nail) returns the suitable calendar events

--- src/main/java/com/macanails/macanailsappointmentbooker/service/ReservationService.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/service/ReservationService.java
@@ -1,5 +1,6 @@
 package com.macanails.macanailsappointmentbooker.service;
 
+import com.google.api.client.util.DateTime;
 import com.macanails.macanailsappointmentbooker.model.CalendarEvent;
 import com.macanails.macanailsappointmentbooker.model.Nail;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,12 +19,31 @@ public class ReservationService {
     @Autowired
     CalendarService calendarService;
 
+    @Autowired
+    NailService nailService;
 
-    public List<CalendarEvent> getFreeSlots(Nail nail) {
-        return null;
+    public List<CalendarEvent> getFreeSlots(Nail nail) throws IOException {
+        int neededHours= nailService.calculateNeededTime(nail);
+        System.out.println(new DateTime(System.currentTimeMillis()));
+        LocalDateTime now = LocalDateTime.now().withNano(0);
+        LocalDateTime three = now.plusMonths(3);
+
+        DateTime threeMothLater = DateTimeService.convertLocalDateTimeToDateTime(three);
+        System.out.println(threeMothLater);
+        List<CalendarEvent> freeEvents = calendarService.getFreeEvents(DateTimeService.convertLocalDateTimeToDateTime(now), threeMothLater);
+        List<CalendarEvent> suitableEvents = new ArrayList<>();
+        for (int i = 0; i < freeEvents.size()-neededHours; i++) {
+            if(freeEvents.get(i+neededHours).getEndTime() == freeEvents.get(i).getStartTime().plusHours(neededHours)){
+                freeEvents.get(i).setNail(nail);
+                suitableEvents.add(freeEvents.get(i));
+            }
+
+        }       return suitableEvents;
     }
 
     public String  saveAppointment(CalendarEvent calendarEvent) throws IOException {
+
+
         LocalDateTime startTime =calendarEvent.getStartTime();
         LocalDateTime endTime = calendarEvent.getStartTime().plusHours(calendarEvent.getNeededTime());
         List<CalendarEvent> freeSlots = calendarService.getFreeEvents(DateTimeService.convertLocalDateTimeToDateTime(startTime),
