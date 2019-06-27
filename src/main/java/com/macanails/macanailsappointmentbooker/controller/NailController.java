0d6ab3f21aa7a29bc1b9a01ce3c87947a6668a9b
Szajler Indira <szajler.indira@gmail.com>
2019-06-27 13:30:17 +0200

    getFreeSlots method returns a map of calendar events separated by date

--- src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/controller/NailController.java
@@ -3,43 +3,47 @@ package com.macanails.macanailsappointmentbooker.controller;
 
 import com.macanails.macanailsappointmentbooker.model.*;
 import com.macanails.macanailsappointmentbooker.service.CalendarService;
+import com.macanails.macanailsappointmentbooker.service.DateTimeService;
 import com.macanails.macanailsappointmentbooker.service.NailService;
 import com.macanails.macanailsappointmentbooker.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @CrossOrigin
 public class NailController {
     @Autowired
     NailService nailService;
-//    @Autowired
-//    Nail nail;
     @Autowired
     ReservationService reservationService;
-
     @Autowired
     CalendarService calendarService;
 
     @PostMapping(value = "/")
     public Map<String, List<CalendarEvent>> getFreeSlots(@RequestBody Map<String, String> answers) throws IOException {
         System.out.println(answers);
-       Nail nail= Nail.builder()
+        Nail nail = Nail.builder()
                 .decor(Decoration.valueOf(answers.get("decoration")))
                 .shape(Shape.valueOf(answers.get("shape")))
                 .type(Type.valueOf(answers.get("nailType")))
-               .build();
+                .build();
 
         List<CalendarEvent> freeSlots = reservationService.getFreeSlots(nail);
-        Map <String, List<CalendarEvent>> freeSlotsMap = new HashMap<String, List<CalendarEvent>>();
-        freeSlotsMap.put("freeSlots",freeSlots );
+        Map<String, List<CalendarEvent>> freeSlotsMap = new HashMap<String, List<CalendarEvent>>();
+        List<CalendarEvent> events;
+
+        for (CalendarEvent freeSlot : freeSlots) {
+            String dateMapKey = DateTimeService.convertLocalDateTimeToDateString(freeSlot);
+            events = freeSlotsMap.computeIfAbsent(dateMapKey, k -> new ArrayList<>());
+            events.add(freeSlot);
+        }
+
         return freeSlotsMap;
     }
+
 
 //    @PostMapping(value = "/personal")
 //    public Map<String, String> getPersonal(@RequestBody Map<String, String> personal) {
