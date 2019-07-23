79fd3abea32c3ea7ffbde89a734a6446a73a8cdb
NoemiSz <szokenoemijob@gmail.com>
2019-07-23 10:28:45 +0200

    Decoration enum was created.

--- src/main/java/com/macanails/macanailsappointmentbooker/model/Decoration.java
+++ src/main/java/com/macanails/macanailsappointmentbooker/model/Decoration.java
@@ -1,17 +1,24 @@
 package com.macanails.macanailsappointmentbooker.model;
 
 public enum Decoration {
-    YES  (3),
-    NO (1);
+    TRANSFERFÓLIA_VAGY_CUKORPOR("Transzferfólia vagy cukorpor", 0.084f),
+    EFFEKT_POROK("Effekt porok", 0.17f),
+    INDA_MINTA("Inda minta", 0.17f),
+    FESTETT_3D_VIRÁGOK("Festett 3D virágok", 0.33f),
+    KÖVEK("Kövek", 0.25f),
+    KOMPLEX_DÍSZÍTÉS("Komplex Díszítés", 0.5f),
+    REALISZTIKUS_FESTÉS("Realisztikus festés", 1f),
+    NINCS_DÍSZÍTÉS("Nincs díszítés",0f);
 
+    private final String description;
+    private final float time;
 
-    private final int weight;
-
-    Decoration(int weight) {
-        this.weight = weight;
+    Decoration(String description, float time) {
+        this.description = description;
+        this.time = time;
     }
 
-    public int getWeight() {
-        return this.weight;
+    public float getTime() {
+        return this.time;
     }
 }
